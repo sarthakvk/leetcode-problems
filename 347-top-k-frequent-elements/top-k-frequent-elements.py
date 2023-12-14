b55bcf9cd1e671ad7c1560830e8735f596893f30
Sarthak <kumar.v.sarthak@gmail.com>
2023-12-15 00:59:49 +0530

    Time: 101 ms (46.46%) | Memory: 22.3 MB (7.64%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -7,20 +7,13 @@ class Solution:
             c[i] += 1
 
         arr = [[] for i in range(len(nums)+1)]
-        print(len(arr), arr)
         for num, cnt in c.items():
             arr[cnt].append(num)
         
         ans = []
 
         for i in arr[::-1]:
-            if len(ans) == k:
-                break
             for j in i:
-                if len(ans) == k:
-                    break
                 ans.append(j)
-
-
-        return ans
-
+                if len(ans) == k:
+                    return ans
