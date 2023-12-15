a0230f2f4d657bb565540340b9270b2f757e210e
Sarthak <kumar.v.sarthak@gmail.com>
2023-12-15 16:01:13 +0530

    Time: 468 ms (44.19%) | Memory: 27.1 MB (72.14%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.py
+++ 310-minimum-height-trees/minimum-height-trees.py
@@ -8,14 +8,17 @@ class Solution:
             adj.setdefault(i[0],{})[i[1]] = True
             adj.setdefault(i[1],{})[i[0]] = True
         
+        nln = adj.keys()
         while len(adj.keys()) > 2:
             ln = []
-            for i in adj.keys():
+
+            for i in nln:
                 if len(adj[i].keys()) == 1:
                     ln.append(i)
-            
+            nln = set()
             for i in ln:
                 k = list(adj[i].keys()).pop()
+                nln.add(k)
                 del adj[i]
                 del adj[k][i]
         
