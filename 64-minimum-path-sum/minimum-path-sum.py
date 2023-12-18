0ecb3fb7365088d69432c4845ef23da04f32df5e
Sarthak <kumar.v.sarthak@gmail.com>
2023-12-18 23:00:04 +0530

    Time: 89 ms (80.02%) | Memory: 18.1 MB (30.40%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,26 +1,18 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        e = (len(grid)-1,len(grid[0])-1)
-        dp = [[float('inf') for i in range(e[1]+1)] for j in range(e[0]+1)]
-
-        def sol(x, y):
-            if x == e[0] and y == e[1]:
-                dp[x][y] = grid[x][y]
-                return grid[x][y]
-            elif x > e[0] or y > e[1]:
-                return float('inf')
-            elif dp[x][y] != float('inf'):
-                return dp[x][y]
-
-            p1 = x+1, y
-            p2 = x, y+1
-
-            ans = min(sol(*p1), sol(*p2)) + grid[x][y]
-            dp[x][y] = ans
-            return ans
-
-        return sol(0,0)
-
-
-
+            
         
+        m, n = len(grid), len(grid[0])
+        
+        for i in range(1, m):
+            grid[i][0] += grid[i-1][0]
+        
+        for i in range(1, n):
+            grid[0][i] += grid[0][i-1]
+        
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        
+        return grid[-1][-1]
