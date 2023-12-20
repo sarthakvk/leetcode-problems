cb26771ab810926607c746e5017f85cb923943c7
Sarthak <kumar.v.sarthak@gmail.com>
2023-12-20 21:45:42 +0530

    Time: 37 ms (63.35%) | Memory: 17.4 MB (26.20%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -2,14 +2,16 @@ class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         wl = s.split(" ")
         wm = {}
+        mw = {}
+
         if len(pattern) != len(wl):
             return False
-        if len(set(pattern)) != len(set(wl)):
-            return False
+
         for w, pat in zip(wl, pattern):
-            if pat not in wm:
+            if pat not in wm and w not in mw:
                 wm[pat] = w
-            elif wm[pat] != w:
+                mw[w] = pat
+            elif wm.get(pat, '') != w or mw.get(w, '') != pat:
                 return False
         
         return True
