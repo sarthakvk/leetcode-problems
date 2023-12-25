d60fa35031705fbb328cc6dd857fc56d28cd6e73
Sarthak <kumar.v.sarthak@gmail.com>
2023-12-25 21:27:10 +0530

    Time: 32 ms (89.72%) | Memory: 17.2 MB (5.23%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,39 +6,18 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        def inorder(root, ans):
-            if not root:
-                ans.append(None)
-                return
-
-            inorder(root.left, ans)
-            ans.append(root.val)
-            inorder(root.right, ans)
-            return ans
-
-        def preorder(root, ans):
-            if not root:
-                ans.append(None)
-                return
-            
-            ans.append(root.val)
-            preorder(root.left, ans)
-            preorder(root.right, ans)
+        if (p is None and q is None):
+            return True
         
-        p1, p2, i1, i2 = [],[],[],[]
-
-        inorder(p, i1)
-        inorder(q, i2)
-        preorder(p, p1)
-        preorder(q, p2)
-
-        if len(p1) != len(p2):
-            return False
+        elif (p and q):
+            return (
+                p.val == q.val and 
+                self.isSameTree(p.left, q.left) and 
+                self.isSameTree(p.right, q.right)
+            )
         
-        for i, j, k, l in zip(p1, p2, i1, i2):
-            if not ((i == j) and (k == l)):
-                return False
-        return True
+        return False
+    
 
 
         
