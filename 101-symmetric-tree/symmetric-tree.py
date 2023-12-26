297fa01eb8d25fdcc1498eac3bfb8785f1b3674c
Sarthak <kumar.v.sarthak@gmail.com>
2023-12-27 03:12:43 +0530

    Time: 41 ms (56.31%) | Memory: 17.5 MB (5.47%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -6,30 +6,15 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        m = {}
-        def bfs(root, lvl=0):
-            if not root:
-                l = m.setdefault(lvl, [])
-                l.append(root)
-                return
-            
-            l = m.setdefault(lvl, [])
-            l.append(root.val)
-            bfs(root.left, lvl=lvl+1)
-            bfs(root.right, lvl=lvl+1)
-        
-        bfs(root)
-        for k, v in m.items():
-            i, j = 0, len(v)-1
-
-            while i < j:
-                if v[i] != v[j]:
+        def sym(a, b):
+            if a and b:
+                if a.val == b.val:
+                    return sym(a.left, b.right) and sym(a.right, b.left)
+                else:
                     return False
-                i += 1
-                j -= 1
-        return True
-
-            
-            
-
+            else:
+                return a == b
+        
+        return sym(root.left, root.right)
+        
         
