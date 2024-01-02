ab3b372f6602b1be06870612683d8b6808a6b4db
Sarthak <kumar.v.sarthak@gmail.com>
2024-01-02 15:40:35 +0530

    Time: 345 ms (5.01%) | Memory: 29.2 MB (11.36%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.py
@@ -0,0 +1,47 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        m = len(board)
+        n = len(board[0])
+
+        def bfs(x, y, nmap):
+            if 0 > x or x >= m or y < 0 or y >= n:
+                return
+            if board[x][y] == 'X' or (x, y) in nmap:
+                return
+            nmap[(x, y)] = True
+            
+            bfs(x+1, y, nmap)
+            bfs(x-1, y, nmap)
+            bfs(x, y+1, nmap)
+            bfs(x, y-1, nmap)
+
+            if x == 0 or y == 0 or x + 1 == m or y+1 == n:
+                nmap["block"] = True
+            
+
+        visited = {}
+        blocked = {}
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if board[i][j] == 'X':
+                    continue
+                
+                if not visited.get((i, j)) and not blocked.get((i,j)):
+                    region = {}
+                    bfs(i, j, region)
+                    block = region.pop('block', False)
+
+                    for x, y in region.keys():
+                        if block:
+                            if (x, y) in blocked:
+                                break
+                            blocked[(x, y)] = True
+                        else:
+                            visited[(x, y)] = True
+                            board[x][y] = 'X'
+                
+            
+            
