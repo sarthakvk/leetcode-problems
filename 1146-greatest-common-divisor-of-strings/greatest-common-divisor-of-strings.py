c11f6e29886f56ec870dfe25be786bc7fbff6e8f
Sarthak <kumar.v.sarthak@gmail.com>
2024-01-05 11:42:21 +0530

    Time: 29 ms (97.38%) | Memory: 17.5 MB (13.46%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,18 +1,9 @@
+from math import gcd
+
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        def matchpat(s, pat):
-            if len(s) % len(pat) != 0:
-                return False
-            
-            for i in range(len(s)):
-                if s[i] != pat[i%len(pat)]:
-                    return False
-            return True
-        ans = ""
-        for i in range(min(len(str1), len(str2))):
-            if str1[:i+1] != str2[:i+1] or not matchpat(str1, str1[:i+1]) or not matchpat(str2, str1[:i+1]):
-                continue
-            ans = str1[:i+1]
-        return ans
+        if str1 + str2 != str2+str1:
+            return ""
+        return str1[:gcd(len(str1), len(str2))]
 
         
