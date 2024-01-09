a7b2dbb7aa886632407666f1170a1247eed75625
Sarthak <kumar.v.sarthak@gmail.com>
2024-01-09 23:49:54 +0530

    Time: 57 ms (95.60%) | Memory: 24.1 MB (10.64%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,4 +1,17 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        return [i.bit_count() for i in range(n+1)]
+        dp = [0]*(n+1)
+
+        cmp = 1
+        start = 1
+        for i in range(1, n+1):
+            if i < cmp:
+                dp[i] = 1 + dp[start]
+                start += 1
+            else:
+                start = 1
+                cmp <<= 1
+                dp[i] = 1
+        
+        return dp
         
