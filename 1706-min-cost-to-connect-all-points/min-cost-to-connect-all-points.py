38d3bc2ae295c716a79bd1b68737fc039282f29d
Sarthak <kumar.v.sarthak@gmail.com>
2024-01-28 15:50:54 +0530

    Time: 2208 ms (43.94%) | Memory: 77.9 MB (85.37%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,52 +1,33 @@
-class Set:
-    def __init__(self, size):
-        self.parent = [i for i in range(size)]
-        self.sub_size = [1]*size
-        self.size = size
-    
-    def find(self, n):
-        while n != self.parent[n]:
-            n = self.parent[n]
-        
-        return n
-    
-    def union(self, n1, n2):
-        r1, r2 = self.find(n1), self.find(n2)
-
-        if self.sub_size[r2] > self.sub_size[r1]:
-            r1, r2 = r2, r1
-        
-        self.parent[r2] = r1
-        self.sub_size[r1] += r2
-    
-    def same(self, n1, n2):
-        r1, r2 = self.find(n1), self.find(n2)
-        return r1 == r2
-
+from heapq import *
 
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        def get_distance(p1, p2):
-            return abs(p1[0]-p2[0]) + abs(p1[1] - p2[1])
-
+        pq = []
         ans = 0
-        distance = []
+        tree = [False]*len(points)
+        def get_distance(p1, p2):
+            return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
 
-        for i in range(len(points)):
-            p1 = points[i]
-            for j in range(i+1, len(points)):
-                p2 = points[j]
-                dist = get_distance(p1, p2)
-                distance.append([dist, i, j])
-        
-        distance.sort(key= lambda x: x[0])
-        s = Set(len(points))
+        v = 0
 
-        for edge in distance:
-            if s.same(edge[1], edge[2]):
-                continue
+        while not tree[v]:
+            tree[v] = True
+
+            for i in range(len(points)):
+                if tree[i]:
+                    continue
+                dist = get_distance(points[v], points[i])
+                heappush(pq, (dist, v, i))
             
-            s.union(edge[1], edge[2])
-            ans += edge[0]
+            if not pq:
+                break
+
+            nxt_edge = heappop(pq)
+            
+            while pq and tree[nxt_edge[2]]:
+                nxt_edge = heappop(pq)
+            if not tree[nxt_edge[2]]:
+                ans += nxt_edge[0]
+                v = nxt_edge[2]
         
         return ans
