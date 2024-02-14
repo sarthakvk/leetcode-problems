336d7428908c362c1537867e6d52eb7b55539cc3
Sarthak <kumar.v.sarthak@gmail.com>
2024-02-14 15:42:39 +0530

    Time: 309 ms (42.36%) | Memory: 30.4 MB (66.49%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,8 +1,18 @@
+from math import isinf
+
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        nums = set(nums)        
-        for i in range(1, len(nums)+1):
-            if i not in nums:
-                return i
+        n = len(nums)
+        for i in range(len(nums)):
+            if nums[i] <= 0:
+                nums[i] = float('inf')
+
+        for i in range(len(nums)):
+            if not isinf(nums[i]) and abs(nums[i]) <= n:
+                nums[abs(nums[i])-1] = -abs(nums[abs(nums[i])-1])
+
+        for i, v in enumerate(nums):
+            if v > 0:
+                return i + 1
         
-        return i+1
+        return len(nums) + 1
