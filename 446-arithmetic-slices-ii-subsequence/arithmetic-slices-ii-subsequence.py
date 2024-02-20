0d2c6ec7239a4814f8f46dbba89b8852cf633e6f
Sarthak <kumar.v.sarthak@gmail.com>
2024-02-20 15:46:53 +0530

    Time: 408 ms (58.66%) | Memory: 54.7 MB (91.00%) - LeetSync

--- 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.py
+++ 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.py
@@ -1,16 +1,17 @@
+from collections import defaultdict
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        tc = 0
-        dp = [defaultdict(int) for _ in range(len(nums))]
+        ans = 0
+        dp = [defaultdict(int) for i in range(len(nums))]
 
         for i in range(1, len(nums)):
             for j in range(i):
-                diff = nums[i] - nums[j]
+                diff = nums[j] - nums[i]
                 dp[i][diff] += 1
 
                 if diff in dp[j]:
                     dp[i][diff] += dp[j][diff]
-                    tc += dp[j][diff]
+                    ans += dp[j][diff]
         
-        return tc
+        return ans
         
