f70154922367a6d108ddf72d1cabfa9d00e0a36c
Sarthak <kumar.v.sarthak@gmail.com>
2024-02-20 12:03:26 +0530

    Time: 181 ms (41.75%) | Memory: 20.2 MB (85.22%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.go
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.go
@@ -6,30 +6,25 @@
  *     Right *TreeNode
  * }
  */
-
-func min(a, b int) int {
-    if a < b{
-        return a
-    }
-    return b
-}
-
 func minDepth(root *TreeNode) int {
     if root == nil {
         return 0
-    } else if root.Left == nil && root.Right == nil {
+    }
+
+    if root.Left == nil && root.Right == nil {
         return 1
     }
 
-    dr := (int)(1e6)
-    dl := (int)(1e6)
-    if root.Left != nil{
-        dl = minDepth(root.Left)
+    left := math.MaxInt
+    if root.Left != nil {
+        left = minDepth(root.Left)
     }
 
+    right := math.MaxInt
     if root.Right != nil {
-        dr = minDepth(root.Right)
+        right = minDepth(root.Right)
     }
 
-    return min(dr, dl) + 1
+    return 1 + min(left, right)
+    
 }
